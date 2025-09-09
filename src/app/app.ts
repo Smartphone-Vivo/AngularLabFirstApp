a02aa64eecf8acfdb9436ee0b8cbfe421d42870a
Smartphone-Vivo <aleksejpopov268@gmail.com>
2025-09-09 15:29:52 +0300

    MatTable

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {FormsModule} from '@angular/forms';
 import {StudentEditor} from './components/student-editor/student-editor';
 import {TableStudents} from './components/table-students/table-students';
+import {MatTableStudents} from './components/mat-table-students/mat-table-students';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, StudentEditor, TableStudents],
+  imports: [RouterOutlet, FormsModule, StudentEditor, TableStudents, MatTableStudents],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
