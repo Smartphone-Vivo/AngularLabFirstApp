abf7847c1005eb9e50266dbaaa790ac7aaf1c8c9
Smartphone-Vivo <aleksejpopov268@gmail.com>
2025-09-12 01:26:18 +0300

    ЫЫЫ

--- src/app/components/mat-table-students/mat-table-students.ts
+++ src/app/components/mat-table-students/mat-table-students.ts
@@ -1,6 +1,13 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, inject, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {Student} from '../../models/student';
+import {BaseService} from '../../service/base-service';
+import {MatDialog} from '@angular/material/dialog';
+import {DialogEditWrapper} from '../student-editor/dialog-edit-wrapper/dialog-edit-wrapper';
+import {MatButton, MatButtonModule} from '@angular/material/button';
+import {MatSort, MatSortModule, Sort} from '@angular/material/sort';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
 
 /**
  * @title Table with pagination
@@ -9,45 +16,67 @@ import {MatTableDataSource, MatTableModule} from '@angular/material/table';
   selector: 'mat-table-students',
   styleUrl: 'mat-table-students.scss',
   templateUrl: 'mat-table-students.html',
-  imports: [MatTableModule, MatPaginatorModule],
+  imports: [MatTableModule, MatPaginatorModule, MatButtonModule, MatSortModule],
 })
-export class MatTableStudents implements AfterViewInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+export class MatTableStudents implements OnInit, AfterViewInit {
+  displayedColumns: string[] = ['id', 'name', 'surname'];
+  dataSource = new MatTableDataSource<Student>([]);
+  private _liveAnnouncer = inject(LiveAnnouncer);
 
-  @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
+  constructor(
+    private baseService: BaseService,
+    public dialog: MatDialog
+  ) {
+  }
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+
+  @ViewChild(MatSort) sort!: MatSort;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+
+  ngOnInit() {
+    this.loadTable()
+  }
+
+  loadTable() {
+    this.baseService.getAllStudents().subscribe(students => {
+      this.dataSource.data = students
+    })
+  }
+
+
+  addNewStudent() {
+    const dialogRef = this.dialog.open(DialogEditWrapper, {
+      width: '350px',
+      data: null
+    })
+
+    dialogRef.afterClosed().subscribe((result: Student) => {
+      if (result) {
+        this.baseService.addNewStudent(result).subscribe(() => {
+          this.loadTable()
+        })
+      }
+    })
+
+
   }
 }
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-];
